218058af98c9a838f5231b8bf054b9d87f18ec90
anorv <69910278+anorv@users.noreply.github.com>
2021-03-18 17:22:54 +0200

    fetch

--- 00-basics/003-tests/Udemi/Fetch and Promises/script.js
+++ 00-basics/003-tests/Udemi/Fetch and Promises/script.js
@@ -1,8 +1,8 @@
-//
-// const body = document.querySelector('body');
-// const input = document.querySelector('input');
-// //////////////////////////////////////
-// const div = document.createElement('div');
+
+const body = document.querySelector('body');
+const input = document.querySelector('input');
+//////////////////////////////////////
+const div = document.createElement('div');
 // 1. Fetch and Promises PVZ:
 // const getData = function (country) {
 //   fetch(`https://restcountries.eu/rest/v2/name/${country}`) //Return Promise
@@ -52,8 +52,7 @@
 // // getData('germany');
 ////////////////////
 
-// Search from Crypto by name:
-//
+// Search  Crypto by name:
 // input.addEventListener('keyup', function(e){
 //     fetch('https://api.coinlore.com/api/tickers/')
 //         .then(response => {
@@ -77,24 +76,84 @@
 //     body.appendChild(div);
 // })
 //////////////
-
+// Search by country:
+// input.addEventListener('keyup', function(e){
+//     console.log('start');
+//     fetch('https://restcountries.eu/rest/v2/')
+//         .then(response => {
+//             return response.json();
+//         }).then(results => {
+//         div.innerHTML = '';
+//         const search = e.target.value;
+//       //   const filteredResults = results.filter((resultEntry) => {
+//       //       return resultEntry.name === e.target.value;
+//       //
+//       //   })
+//       // console.log(filteredResults);
+//         results.forEach(element => {
+//             console.log(element.name);
+//             const h3 = document.createElement('h3');
+//             h3.innerHTML = element.name;
+//             if (element.name.toLowerCase().includes(search.toLowerCase())) {
+//                 div.appendChild(h3);
+//             }
+//         });
+//     }).catch(error => {
+//         console.log(error);
+//     })
+//     body.appendChild(div);
+// })
 // Search by coordinates:
+//
+// const whereAmI = function (lat, lng) {
+//     fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`) //Return Promise
+//         .then(response => {
+//             // 403 err
+//             if(!response.ok){
+//                 throw new Error( `Problem with geocoding (${response.status})`)
+//             }
+//             return response.json()
+//         })
+//         .then(data => {
+//             console.log(`You are in ${data.city},${data.country}`);
+//         })
+//         .catch(err => {
+//             console.error(err.message);
+//         });
+// };
+//
+// whereAmI(19.037, 72.873);
 
-const whereAmI = function (lat, lng) {
-    fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`) //Return Promise
-        .then(response => {
-            // 403 err
-            if(!response.ok){
-                throw new Error( `Problem with geocoding (${response.status})`)
-            }
-            return response.json()
-        })
-        .then(data => {
-            console.log(`You are in ${data.city},${data.country}`);
-        })
-        .catch(err => {
-            console.error(err.message);
-        });
-};
+// PROMISES PVZ:
 
-whereAmI(19.037, 72.873);
+// const p = Promise.resolve([1,2,3]);
+// p.then(function(v) {
+//     console.log(v[0]); // 1
+// });
+//
+// p.then(function(v) {
+//     console.log(v[2]); // 1
+// });
+
+
+// ASYNC / AWAIT
+
+// const con = async function(country){
+//  const res =  await fetch('https://api.coinlore.com/api/tickers/');
+//
+//     console.log(res);
+// }
+// con();
+// console.log('First');
+//
+
+// ASYNC / AWAIT 2
+const bit = async function(country){
+    const res =  await fetch('https://api.coinlore.com/api/tickers/');
+    const data = res.json();
+    console.log(data);
+
+}
+
+bit();
+console.log('First');
