f9f1d9f0f658c709e99f0a3dca3788be33651db2
anorv <69910278+anorv@users.noreply.github.com>
2021-03-23 10:48:55 +0200

    API

--- 00-basics/003-tests/Udemi/node/API/index.js
+++ 00-basics/003-tests/Udemi/node/API/index.js
@@ -43,25 +43,52 @@ const url = require('url');
 
 
 // ///////////////////////////////////////////////////////////////////
+//SERVER / simple API
+const replaceTemplate = (temp,product) =>{
+let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+output = output.replace(/{%IMAGE%}/g, product.image);
+output = output.replace(/{%PRICE%}/g, product.price);
+output = output.replace(/{%FROM%}/g, product.from);
+output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+output = output.replace(/{%QUANTITY%}/g, product.nutrients);
+output = output.replace(/{%DESCRIPTION%}/g, product.descriptionn);
+output = output.replace(/{%ID%}/g, product.id);
 
-//SERVER
+if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic'); //by class name
+ return output;
+}
+
+
+
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-const productData = JSON.parse(data);
+const dataObj = JSON.parse(data);
 
 
 const server = http.createServer((req, res) => {
     const pathName = req.url;
+
+
+    // Overview page
     if (pathName === '/' || pathName === '/overview') {
-        res.end('this is the Overview');
+        res.writeHead(200, {'Content-type': 'text/html'});
+
+        // loop through data and return array
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el));
+       const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+        res.end(output);
+
+        // Product page
     }else if (pathName === '/product') {
         res.end('this is the Product');
         // http://127.0.0.1:8000/api
     }else if (pathName === '/api'){
-        // fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err,data)=> {
-        //     const productData = JSON.parse(data);
-        //     console.log(productData);
-            res.writeHead(200, {'Content-type': 'application/json'});
-            res.end(data);
+        res.writeHead(200, {'Content-type': 'application/json'});
+        res.end(data);
+        // Not found
     }else{
         res.writeHead(404, {
             'Content - type': 'text/html',
