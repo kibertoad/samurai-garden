8c9c331449dfa31dd9454877f15dc70738dd2bda
anorv <69910278+anorv@users.noreply.github.com>
2021-03-29 16:46:43 +0300

    task-tracker

---
+++ 00-basics/003-tests/Udemi/vue/03.29/task-tracker/test/src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+<header>
+  <h1>{{title}}</h1>
+  <Button @btn-click= "$emit('toggle-add-task')"
+          :text="showAddTask ? 'Close' : 'Add task'"
+          :color="showAddTask ? 'red' : 'green'"/>
+<!--  <Button  text="Update Task" color="blue"/>-->
+<!--  <Button  text="Delete Task" color="red"/>-->
+</header>
+</template>
+
+<script>
+import Button from "@/components/Button";
+
+export default {
+name: "Header.vue",
+  props: {
+  title: String,
+  showAddTask: Boolean
+  },
+  components: {
+    Button,
+  }
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 10px 200px;
+}
+</style>
